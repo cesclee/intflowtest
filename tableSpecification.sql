51794601b3a989de4c413e177f6aeea69240444f
Changi_Fowler <33275258+cesclee@users.noreply.github.com>
2021-02-04 16:41:19 +0900

    tableSpecification
    테이블명세서

---
+++ tableSpecification.sql
@@ -0,0 +1,13 @@
+SELECT ORDINAL_POSITION 'count_number', 
+table_name'tablename',
+COLUMN_NAME 'fieldname',
+ DATA_TYPE 'data TYPE',
+ COLUMN_TYPE 'data LENGTH',
+ COLUMN_KEY 'KEY',
+ IS_NULLABLE 'nullable',
+ EXTRA 'AutoIncrease', 
+ COLUMN_DEFAULT 'default'
+ 
+ FROM INFORMATION_SCHEMA.COLUMNS
+ WHERE TABLE_SCHEMA = 'task01_intflowtest'
+ ORDER BY TABLE_NAME, ORDINAL_POSITION;
